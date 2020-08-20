0d8fe730d0a451a82bf552d5ae487e193592d033
nachonitz <initz@fi.uba.ar>
2020-08-20 15:31:02 -0300

    Updates

---
+++ precache-manifest.1e72cb58167e298340537453b9b34c54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55152d90cf119d03bf6db66b95d7b002",
+    "url": "/instagram-stats/index.html"
+  },
+  {
+    "revision": "5c350f869c1ad1d269ad",
+    "url": "/instagram-stats/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d78c3b999db3f0762186",
+    "url": "/instagram-stats/static/js/2.109d8e16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/instagram-stats/static/js/2.109d8e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c350f869c1ad1d269ad",
+    "url": "/instagram-stats/static/js/main.22fb311d.chunk.js"
+  },
+  {
+    "revision": "91c1ac10005c9a7ef562",
+    "url": "/instagram-stats/static/js/runtime-main.4d9bdda5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/instagram-stats/static/media/logo.5d5d9eef.svg"
+  }
+]);
