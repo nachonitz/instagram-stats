a82f830a100dc2c10ad5af94a873acef78d9b4b7
nachonitz <initz@fi.uba.ar>
2020-08-24 02:39:28 -0300

    Updates

---
+++ precache-manifest.6e63b438af22dc2154805a34805e8312.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0cccf4d973677de6c95e71d49138316",
+    "url": "/instagram-stats/index.html"
+  },
+  {
+    "revision": "a697b6bce1cca1ba4f6f",
+    "url": "/instagram-stats/static/css/main.6bccfe1a.chunk.css"
+  },
+  {
+    "revision": "7ab0353a18695af38394",
+    "url": "/instagram-stats/static/js/2.e92086a5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/instagram-stats/static/js/2.e92086a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a697b6bce1cca1ba4f6f",
+    "url": "/instagram-stats/static/js/main.1eb31d83.chunk.js"
+  },
+  {
+    "revision": "91c1ac10005c9a7ef562",
+    "url": "/instagram-stats/static/js/runtime-main.4d9bdda5.js"
+  },
+  {
+    "revision": "258751f840975289089eee82211d0ba2",
+    "url": "/instagram-stats/static/media/verified.258751f8.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/instagram-stats/precache-manifest.dceacd22e4f9185ee796ca4589387fd7.js"
+  "/instagram-stats/precache-manifest.6e63b438af22dc2154805a34805e8312.js"
 );
 
 self.addEventListener('message', (event) => {
